ac91d26480819a77960b4c30fc0f323e35d2222a
Mayank Dave <127477037+davemayank10@users.noreply.github.com>
2023-04-25 10:23:42 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Pattern2{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		// System.out.println("enter rows:");
+		// int r=sc.nextInt();
+		// System.out.println("enter columns:");
+		// int c=sc.nextInt();
+		int row=1;
+		while(row<=5)
+		{
+			int column=5;
+			while(column>=row)
+			{
+				System.out.print("*"+" ");
+				column--;
+			}
+			//column--;
+			System.out.println();
+			row++;
+		}
+	}
+}
