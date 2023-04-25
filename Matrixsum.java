0eb766b30adac469831c2527f6c1957b3d8639f1
Mayank Dave <127477037+davemayank10@users.noreply.github.com>
2023-04-25 10:28:47 +0530

    Add files via upload

---
+++ Matrixsum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Matrixsum{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter no. of rows:");
+		int r=sc.nextInt();
+		System.out.println("enter no. of columns:");
+		int c=sc.nextInt();
+		for()
+	}
+}
